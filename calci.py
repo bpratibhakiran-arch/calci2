d2f30d73ac2133617d4171b9f2796a323761049a
bpratibhakiran-arch <bpratibhakiran@gmail.com>
2026-05-20 23:28:55 +0530

    Add files via upload

--- calci.py
+++ calci.py
@@ -4,12 +4,20 @@ st.title("Calculator")
 
 num1 = st.number_input("Enter first no. : ")
 num2 = st.number_input("Enter second no. : ")
-add = st.sidebar.button("Add")
-sub = st.sidebar.button("Subtract")
-mul = st.sidebar.button("Multiply")
-div = st.sidebar.button("Divide")
-pow = st.sidebar.button("Power")
-per = st.sidebar.button("Percentage")
+
+col1, col2, col3, = st.columns(3)
+
+with col1:
+    add = st.button("Add")
+    div = st.button("Divide")
+
+with col2:
+    sub = st.button("Subtract")
+    pow = st.button("Power")
+
+with col3:
+    mul = st.button("Multiply")
+    per = st.button("Percentage")
 
 if add:
     result = (num1 + num2)
@@ -31,6 +39,18 @@ elif pow:
     result = (num1 ** num2)
     st.markdown(f"""Answer = {result}""")
 
+elif per:
+    result = ((num1 / 100) * num2)
+    st.markdown(f"""Answer = {result}""")
+
+elif per:
+    result = ((num1 / 100) * num2)
+    st.markdown(f"""Answer = {result}""")
+
+elif per:
+    result = ((num1 / 100) * num2)
+    st.markdown(f"""Answer = {result}""")
+    
 elif per:
     result = ((num1 / 100) * num2)
     st.markdown(f"""Answer = {result}""")
